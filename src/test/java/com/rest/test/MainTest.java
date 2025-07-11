86c2e65af1d5501a61760482d2759b64c0a00408
ShikshaQA <adhikarishiksha78@gmail.com>
2025-07-11 17:26:35 -0400

    Moved MainTest.java to src/test/java/com/rest/test directory

--- src/test/java/com/rest/test/MainTest.java
+++ src/test/java/com/rest/test/MainTest.java
@@ -1,16 +1,11 @@
 package com.rest.test;
 
-import io.restassured.RestAssured;
+
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
-
-import static org.hamcrest.Matchers.*;
-
-import java.util.List;
-
 import org.testng.annotations.Test;
-
+import static org.hamcrest.Matchers.*;
 import static io.restassured.RestAssured.given;
 
 @Test
@@ -19,54 +14,52 @@ public class MainTest {
     RequestSpecification requestSpecification;
     Response response;
     ValidatableResponse validatableResponse;
+    
     @Test
-    public void verifyStatusCode() {
+    public void testCreateBook() {
+        String requestBody = "{\n" +
+                "    \"name\": \"A to the Bodhisattva Way of Life\",\n" +
+                "    \"author\": \"Santideva\",\n" +
+                "    \"price\": 15.41\n" +
+                "}";
 
-        // Base URL of the API
-        RestAssured.baseURI = "http://localhost:8085/books";
-
-        // Username and password for Basic Authentication
-        String username = "user"; // Replace with the correct username
-        String password = "password"; // Replace with the correct password
-
-        // Create the request specification
-        RequestSpecification requestSpecification = given()
-                .auth().preemptive().basic(username, password) // Use preemptive basic auth
-                .log().all(); // Log all request details (headers, body, etc.)
-
-        // Send GET request and get the response
-        Response response = requestSpecification.get();
-
-        // Print the response details for debugging
-        System.out.println("Response Status Code: " + response.getStatusCode());
-        System.out.println("Response Body: " + response.prettyPrint());
-        System.out.println("Response Headers: " + response.getHeaders());
-
-        // Perform validation on the response
-        ValidatableResponse validatableResponse = response.then();
-
-        /* Validate status code */
-        validatableResponse.statusCode(200);
-
-        // Validate status line
-        validatableResponse.statusLine("HTTP/1.1 200 ");
-    }
-    @Test
-    public void testGetBooks() {
         Response response = given()
-                                .auth().basic("user", "password")
+                                .auth().basic("admin", "password")
+                                .contentType("application/json")
+                                .body(requestBody)
+                            .when()
+                                .post("http://localhost:8085/books")
+                            .then()
+                                .statusCode(201)
+                                .extract().response();
+
+        // Validate the response body
+        response.then().body("name", equalTo("A to the Bodhisattva Way of Life"))
+                .body("author", equalTo("Santideva"))
+                .body("price", equalTo(15.41f));
+    }
+    
+    @Test
+    public void testGetBookById() {
+        int bookId =2;
+
+        Response response = given()
+                                .auth().basic("admin", "password")
                                 .contentType("application/json")
                             .when()
-                                .get("http://localhost:8085/books")
+                                .get("http://localhost:8085/books/" + bookId)
                             .then()
                                 .statusCode(200)
                                 .extract().response();
-        // Optionally, validate the first book's details
-        response.then() .body("[1].name", equalTo("The Life-Changing Magic of Tidying Up"))
-        .body("[1].author", equalTo("Marie Kondo"));
-    }
 
-    
+        // Print for debugging
+        System.out.println("Response: " + response.getBody().asString());
+        // Validate the book details
+        response.then().body("id", equalTo(bookId))
+                .body("name", equalTo("The Life-Changing Magic of Tidying Up"))
+                .body("author", equalTo("Marie Kondo"))
+                .body("price", equalTo(9.69f));
+    }
 
 
 }
